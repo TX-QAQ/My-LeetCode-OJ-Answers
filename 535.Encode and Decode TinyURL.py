f0bda779605a1d17c0c498a9dacc3a93a2c67092
TX <tangxinrt@gmail.com>
2017-04-21 14:43:11 +0800

    Add some explanations for this algorithm

--- 535.Encode and Decode TinyURL.py
+++ 535.Encode and Decode TinyURL.py
@@ -1,6 +1,13 @@
 from re import sub
 class Codec:
-
+    """
+    encode the str char by char, repeating char will be changed into \n\c : c is the char while n is the number
+    for example: aaabbbbc is encoded into \3\a\4\bc
+    Some questions left for inspiring thinking:
+        1.Why we don't encode them as \c\n ?
+        2.Is so much "\" really needed?
+    
+    """
     def encode(self, longUrl):
         sub1 = sub(r"\\", r"\\\\", longUrl)
         return sub(r'([^\\])\1+', lambda m: "\\" + str(len(m.group(0))) + "\\" + m.group(1), sub1)
